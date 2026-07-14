6f658b18bd21107e553d22bdf2e58dfbec89dadf
SamariddinNorboyev <samariddin.grex@gmail.com>
2026-07-14 15:58:40 +0500

    a

--- tgbot/states/bot_states.py
+++ tgbot/states/bot_states.py
@@ -3,4 +3,5 @@ from aiogram.fsm.state import State, StatesGroup
 class BotState(StatesGroup):
     waiting_for_agreement = State()
     waiting_for_credentials = State()
+    waiting_for_username = State()
     ready_for_links = State()
